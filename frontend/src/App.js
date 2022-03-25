3d12a849bc4d24fff5eaaa12a99d7402c6ff4ca3
Nikon Podgorny <podgorny.nikon@yandex.ru>
2022-03-25 11:58:17 +0300

    React example tests

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { NavLink, Link, Outlet, useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import { NavLink, Link, Outlet, useNavigate } from "react-router-dom";
 
 function App() {
   let [user, setUser] = useState(null);
@@ -8,36 +8,39 @@ function App() {
   return (
     <div className="App">
       <header>
-        <Link to="/"><h1>Tuna</h1></Link>
+        <Link to="/">
+          <h1>Tuna</h1>
+        </Link>
       </header>
       <nav>
         <NavLink
           // TODO добавить стили и изменить style на className
           style={({ isActive }) => {
-            return {color: isActive ? "red" : ""}
+            return { color: isActive ? "red" : "" };
           }}
           to="/history"
         >
           My History
-        </NavLink> {" - "}
-        { user ?
-          <button onClick={() => {
-            setUser(null);
-            navigate("/");
-          }}
-          >
-            {user}: Log out
-          </button> :
-          <NavLink
-            // TODO добавить стили и изменить style на className
-            style={({ isActive }) => {
-              return { color: isActive ? "red" : "" };
-            }}
-            to="/login"
-          >
-            Login
-          </NavLink>
-        }
+        </NavLink>{" "}
+        {" - "}
+        {user
+          ? <button
+              onClick={() => {
+                setUser(null);
+                navigate("/");
+              }}
+            >
+              {user}: Log out
+            </button>
+          : <NavLink
+              // TODO добавить стили и изменить style на className
+              style={({ isActive }) => {
+                return { color: isActive ? "red" : "" };
+              }}
+              to="/login"
+            >
+              Login
+            </NavLink>}
       </nav>
       <Outlet context={[user, setUser]} />
     </div>
--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,6 +1,25 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, shallow } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 test("Example test", () => {
   expect(1).toEqual(1);
+});
+
+test("React render Ok", () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+});
+
+test("Render Tuna", () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  const linkElement = screen.getByText(/Tuna/i);
+  expect(linkElement).toBeInTheDocument();
 });
