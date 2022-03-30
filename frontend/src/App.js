27ef537377a53336a306047e4c2288a46046e25e
JenkaCool <cool.diatel@yandex.ru>
2022-03-31 00:36:55 +0300

    удалены файлы для среды разработки

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,41 +8,47 @@ function App() {
 
   return (
     <div className="App">
-      <header>
-        <Link to="/">
-          <h1>Tuna</h1>
-        </Link>
-      </header>
-      <nav>
-        <NavLink
-          // TODO добавить стили и изменить style на className
-          style={({ isActive }) => {
-            return { color: isActive ? "red" : "" };
-          }}
-          to="/history"
-        >
-          My History
-        </NavLink>{" "}
-        {" - "}
-        {user
-          ? <button
-              onClick={() => {
-                setUser(null);
-                navigate("/");
-              }}
-            >
-              {user}: Log out
-            </button>
-          : <NavLink
-              // TODO добавить стили и изменить style на className
-              style={({ isActive }) => {
-                return { color: isActive ? "red" : "" };
-              }}
-              to="/login"
-            >
-              Login
-            </NavLink>}
-      </nav>
+      <div id="top-bar">
+        <div id="big-top-bar">
+        <header>
+          <Link to="/">
+            <h1>Tuna</h1>
+          </Link>
+        </header>
+        <nav>
+          <NavLink
+            // TODO добавить стили и изменить style на className
+            style={({ isActive }) => {
+              return { color: isActive ? "red" : "" };
+            }}
+            to="/history"
+          >
+            My History
+          </NavLink>{" "}
+          {" - "}
+          {user
+            ? <button
+                onClick={() => {
+                  setUser(null);
+                  navigate("/");
+                }}
+              >
+                {user}: Log out
+              </button>
+            : <NavLink
+                // TODO добавить стили и изменить style на className
+                style={({ isActive }) => {
+                  return { color: isActive ? "red" : "" };
+                }}
+                to="/login"
+              >
+                Login
+              </NavLink>}
+        </nav>
+        </div>
+        <div id="small-top-bar">
+        </div>
+      </div>
       <Outlet context={[user, setUser]} />
     </div>
   );
