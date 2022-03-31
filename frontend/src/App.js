3246d44aa3c3b13d9fd6e488b6729167a4f9be4a
JenkaCool <cool.diatel@yandex.ru>
2022-03-31 20:35:21 +0300

    компоновка элементов панели в одну строку

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,48 +10,49 @@ function App() {
     <div className="App" id="page-content">
       <div id="top-bar">
         <div id="big-top-bar">
-        <header>
-          <Link to="/">
-            <h1 id="logo-tittle">Tuna</h1>
-          </Link>
-        </header>
-        <nav class="sections">
-          <ul>
-          <li>
-            <NavLink
-              // TODO добавить стили и изменить style на className
-              className="navButton"
-              style={({ isActive }) => {
-                return { color: isActive ? "red" : "" };
-              }}
-              to="/history"
-            >
-              My History
-            </NavLink>{" "}
-          </li>
-          <li>
-            {user
-              ? <button className="navButton"
-                  onClick={() => {
-                    setUser(null);
-                    navigate("/");
-                  }}
-                >
-                  {user}: Log out
-                </button>
-              : <NavLink
+          <header>
+            <Link to="/">
+              <h1 id="logo-tittle">Tuna</h1>
+            </Link>
+          </header>
+          <nav class="sections">
+            <ul>
+              <li>
+                <NavLink
                   // TODO добавить стили и изменить style на className
                   className="navButton"
+                  id="historyButton"
                   style={({ isActive }) => {
                     return { color: isActive ? "red" : "" };
                   }}
-                  to="/login"
+                  to="/history"
                 >
-                  Login
-                </NavLink>}
-          </li>
-          </ul>
-        </nav>
+                  My History
+                </NavLink>{" "}
+              </li>
+              <li>
+                {user
+                  ? <button className="navButton"
+                      onClick={() => {
+                        setUser(null);
+                        navigate("/");
+                      }}
+                    >
+                      {user}: Log out
+                    </button>
+                  : <NavLink
+                      // TODO добавить стили и изменить style на className
+                      className="navButton"
+                      style={({ isActive }) => {
+                        return { color: isActive ? "red" : "" };
+                      }}
+                      to="/login"
+                    >
+                      Login
+                    </NavLink>}
+              </li>
+            </ul>
+          </nav>
         </div>
         <div id="small-top-bar">
         </div>
