655f95880409533a0e274a68a34bbb7750b934aa
UgryumovM <60959649+UgryumovM@users.noreply.github.com>
2022-04-16 21:52:24 +0300

    Фикс форматирования

--- frontend/src/components/History.js
+++ frontend/src/components/History.js
@@ -60,31 +60,31 @@ function Table(props) {
 }
 
 function PageButton(props){
-	if(props.curPage === props.id){
-		return (
-		<>
-			<button style={{backgroundColor: 'lightgrey', display: 'inline-block'}} onClick={()=>{props.handler(props.id);}}>{props.id}</button>
-		</>
-		)
-	}
+  if(props.curPage === props.id){
     return (
-	<>
-        <button style={{display: 'inline-block'}} onClick={()=>{props.handler(props.id);}}>{props.id}</button>
-	</>
+      <>
+        <button style={{backgroundColor: 'lightgrey', display: 'inline-block'}} onClick={()=>{props.handler(props.id);}}>{props.id}</button>
+      </>
     )
+  }
+  return (
+    <>
+      <button style={{display: 'inline-block'}} onClick={()=>{props.handler(props.id);}}>{props.id}</button>
+    </>
+  )
 }
 function Footer(props) {
-	var pageList = [];
-	for(var i = 0; i < props.pages; i++){
-		pageList.push(i + 1);
-	}
-	let buttons = pageList.map((i)=> {
-        return <PageButton id={i} handler={props.handler} curPage={props.curPage}/>
-    });
-	return (
-      <div style={{ width:'100%', textAlign: 'center'}}>
-	   {buttons}
-	  </div>);
+  var pageList = [];
+  for(var i = 0; i < props.pages; i++){
+    pageList.push(i + 1);
+  }
+  let buttons = pageList.map((i)=> {
+    return <PageButton id={i} handler={props.handler} curPage={props.curPage}/>
+  });
+  return (
+    <div style={{ width:'100%', textAlign: 'center'}}>
+      {buttons}
+    </div>);
 }
 
 
@@ -102,116 +102,113 @@ export default function History() {
   };
   
   useEffect(() => {
-	  async function GL(user, pass) {
-		
-		var m = JSON.stringify({
-			"username": user, 
-			"password": pass, 
-		});
-		var requestOptions = {
-			mode: 'cors',
-			method: 'POST',
-			headers : {
-				'Accept': 'application/json',
-				'Content-Type' : 'application/json'
-			},
-			body: m
-		};
-		await fetch("/api/login", requestOptions)
-		.then(response => response.json())
-		.then(data => {
-			setBear(data.access_token);
-		});
+    async function GL(user, pass) {	
+      var m = JSON.stringify({
+        "username": user, 
+        "password": pass, 
+      });
+      var requestOptions = {
+        mode: 'cors',
+        method: 'POST',
+        headers : {
+          'Accept': 'application/json',
+          'Content-Type' : 'application/json'
+        },
+        body: m
+      };
+      await fetch("/api/login", requestOptions)
+      .then(response => response.json())
+      .then(data => {
+        setBear(data.access_token);
+      });
     }
     async function GI() {
-		var requestOptions = {
-			mode: 'cors',
-			method: 'GET',
-			headers : {
-				'Accept': 'application/json',
-				'Authorization': 'Bearer ' + bear,
-				'Content-Type' : 'application/json'
-			}
-		};
-		await fetch("/api/get_saves", requestOptions)
-		.then(response => response.json())
-		.then(data => {
-			setIds(data.ids);
-		});
+      var requestOptions = {
+        mode: 'cors',
+        method: 'GET',
+        headers : {
+          'Accept': 'application/json',
+          'Authorization': 'Bearer ' + bear,
+          'Content-Type' : 'application/json'
+        }
+      };
+      await fetch("/api/get_saves", requestOptions)
+      .then(response => response.json())
+      .then(data => {
+        setIds(data.ids);
+      });
     }
 	
-	async function GR() {
-		var curIds = ids.slice((page - 1) * elementsPerPage, page * elementsPerPage);
-		var resData = {"data": []};
+    async function GR() {
+      var curIds = ids.slice((page - 1) * elementsPerPage, page * elementsPerPage);
+      var resData = {"data": []};
 
-		var requestOptions = {
-			mode: 'cors',
-			method: 'GET',
-			headers : {
-				'Accept': 'application/json',
-				'Authorization': 'Bearer ' + bear,
-				'Content-Type' : 'application/json'
-			}
-		};
+      var requestOptions = {
+        mode: 'cors',
+        method: 'GET',
+        headers : {
+          'Accept': 'application/json',
+          'Authorization': 'Bearer ' + bear,
+          'Content-Type' : 'application/json'
+        }
+    };
 		
-		const requests = curIds.map(async (id) => {
-			var name;
+    const requests = curIds.map(async (id) => {
+      var name;
 			
-			await fetch('/api/get_file?id=' + id , requestOptions)
-			.then(response => response.json())
-			.then(data => {
-			name = data.file.filename;
-			});
-			name = name.slice(37);
-			await fetch('/api/get_result?id=' + id , requestOptions)
-			.then(response => response.json())
-			.then(data => {
-				resData.data.push({
-					"id": id,
-					"name": name,
-					"bpm": data.bpm,
-					"tone": data.tone,
-					"dance": data.dance,
-					"energy": data.energy,
-					"happiness": data.happiness,
-					"version": data.version,
-					"date": data.date.slice(5,16)
-					});
-			});
-		});
-		Promise.all(requests).then(() => {
-			resData.data.sort(function (a, b) {
-			  return a.id - b.id;
-			});
-			setData(resData);
-		});
-		
+      await fetch('/api/get_file?id=' + id , requestOptions)
+      .then(response => response.json())
+      .then(data => {
+        name = data.file.filename;
+      });
+      name = name.slice(37);
+      await fetch('/api/get_result?id=' + id , requestOptions)
+      .then(response => response.json())
+      .then(data => {
+        resData.data.push({
+          "id": id,
+          "name": name,
+          "bpm": data.bpm,
+          "tone": data.tone,
+          "dance": data.dance,
+          "energy": data.energy,
+          "happiness": data.happiness,
+          "version": data.version,
+          "date": data.date.slice(5,16)
+        });
+      });
+    });
+    Promise.all(requests).then(() => {
+      resData.data.sort(function (a, b) {
+        return a.id - b.id;
+      });
+      setData(resData);
+      });		
     }
-	if(bear === null) {
-		GL("a", "a");
-	}
-	else if(ids === null){
-		GI();
-	} else {
-		GR();
-	}
-	
+    if(bear === null) {
+      GL("a", "a");
+    }
+    else if(ids === null){
+      GI();
+    } else {
+      GR();
+    }
   }, [page, ids, bear])
 
   if(mdata === null) {
-	  return (
-	  <main className="page-content history-page">
-	  <h1>Loading...</h1>
-	  </main>);
+    return (
+      <main className="page-content history-page">
+        <h1>Loading...</h1>
+      </main>);
   }
   else {
-	  pages = Math.ceil(ids.length / elementsPerPage);
-  return (
-    <main className="page-content history-page">
-		<h2>History!</h2>
-		<Table data={mdata.data} />
-		<Footer handler={updateState} curPage={page} elems={elementsPerPage} pages={pages}/>
-    </main>
-  );
+    pages = Math.ceil(ids.length / elementsPerPage);
+    return (
+      <main className="page-content history-page">
+        <h2>History!</h2>
+        <Table data={mdata.data} />
+        <Footer handler={updateState} curPage={page} elems={elementsPerPage} pages={pages}/>
+      </main>
+    );
   }
 }
