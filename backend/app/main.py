7d7ea263dd27c2661d9a051a4b937d9d63cbe51c
Nikon Podgorny <podgorny.nikon@yandex.ru>
2022-04-21 22:38:35 +0300

    fix models, modules, workers endpoints

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,8 +3,9 @@
 #
 #  Основные эндпоинты приложения
 
-from flask import Blueprint, render_template, current_app
+from flask import Blueprint, render_template, current_app, send_from_directory
 from . import db
+import os
 
 main = Blueprint('main', __name__)
 
@@ -16,9 +17,19 @@ def index():
 ## Возврщает статичный файл из директории static
 @main.route('/<path:path>')
 def static_file(path):
-    return current_app.send_static_file(path)
+    return send_from_directory(current_app.root_path, path)
 
 ## Возврщает файл из директории workers
 @main.route('/workers/<path:path>')
-def static_file(path):
-    return current_app.send_static_file(path)
+def workers_file(path):
+    return send_from_directory(os.path.join(current_app.root_path, 'workers'), path)
+
+## Возврщает файл из директории models
+@main.route('/models/<path:path>')
+def models_file(path):
+    return send_from_directory(os.path.join(current_app.root_path, 'models'), path)
+
+## Возврщает файл из директории modules
+@main.route('/modules/<path:path>')
+def modules_file(path):
+    return send_from_directory(os.path.join(current_app.root_path, 'modules'), path)
